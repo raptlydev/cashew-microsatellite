67a167515d185a925bdde752976ff7953777c434
mainak92 <31222142+mainak92@users.noreply.github.com>
2023-07-20 22:47:02 +0530

    tutorial page

---
+++ server/build/precache-manifest.24de6e56fa5702c4cace8a830776aba7.js
@@ -0,0 +1,271 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce2e7dab3d23f5ac0a4",
+    "url": "/assets/css/14.4e3d2d1f.css"
+  },
+  {
+    "revision": "8946a1777fd0ca613f3a599ddb186cd0",
+    "url": "/assets/images/about-banner.8946a177.jpg"
+  },
+  {
+    "revision": "6e4e3eb9a272022250cc3b2158569f1c",
+    "url": "/assets/images/about.6e4e3eb9.jpg"
+  },
+  {
+    "revision": "2248717864c7892edba3262d75069c51",
+    "url": "/assets/images/crros.22487178.png"
+  },
+  {
+    "revision": "32280ae3fe5d631e256622996cd29320",
+    "url": "/assets/images/test.32280ae3.png"
+  },
+  {
+    "url": "/assets/js/0.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/1.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/10.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/11.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/12.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/14.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/15.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/16.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/17.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/18.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/19.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/2.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/20.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/21.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/22.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/23.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/24.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/25.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/26.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/27.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/28.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/29.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/3.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/30.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/31.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/32.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/33.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/34.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/35.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/36.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/37.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/38.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/39.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/4.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/40.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/41.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/42.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/43.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/44.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/45.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/46.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/47.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/48.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/49.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/5.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/50.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/51.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/52.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/53.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/54.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/55.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/56.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/57.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/58.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/59.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/6.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/60.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/61.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/62.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/63.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/64.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/65.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/66.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/67.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/68.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/69.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/7.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/70.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/71.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/72.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/73.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/74.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/75.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/76.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/77.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/78.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/79.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/8.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/80.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/81.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/82.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/9.4e3d2d1f40f4dd84f811.min.js"
+  },
+  {
+    "url": "/assets/js/app.4e3d2d1f40f4dd84f811.min.js"
+  }
+]);
